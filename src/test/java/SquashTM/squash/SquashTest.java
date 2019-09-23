71b6d95e8ea62706a4d5eed829845255795e8644
FelixTheCat00 <felix.mensahadjei@gmail.com>
2019-09-23 16:24:19 +0200

    First commit

---
+++ src/test/java/SquashTM/squash/SquashTest.java
@@ -0,0 +1,33 @@
+package SquashTM.squash;
+
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+
+
+public class SquashTest {
+	
+	@Test
+	public void SquashTest() throws InterruptedException{
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Installation\\driver\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://192.168.102.95:8089/squash/login");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("admin");
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys("admin");
+		driver.findElement(By.xpath("//*[@id=\"login-form-button-set\"]/input")).click();
+		
+		driver.close();
+	}
+
+}
